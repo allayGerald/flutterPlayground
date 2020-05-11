3b73a261a0e30a636c6fe5880b10576d42c27387
Gerald Simon <gerald@adroittech.co.tz>
2020-05-11 19:57:39 +0300

    Add a button

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,14 +21,30 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/noreen.jpg'),
-              Text('Food Paradise')
-            ],
+        body: Column(children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _products.add('Awesome Product');
+                });
+              },
+              child: Text('Add Product'),
+            ),
           ),
-        ),
+          Column(
+              children: _products
+                  .map((e) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/noreen.jpg'),
+                            Text(e)
+                          ],
+                        ),
+                      ))
+                  .toList())
+        ]),
       ),
     );
   }
