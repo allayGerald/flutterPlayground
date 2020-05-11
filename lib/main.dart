d18816ef31483112773915faf803fd69ffcb59cb
Gerald Simon <gerald@adroittech.co.tz>
2020-05-11 20:55:54 +0300

    Refactoring into separate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'dart:ui';
 
+import 'package:first_app/products_manager.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State {
-  List<String> _products = ['Food Tester'];
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -21,30 +14,7 @@ class _MyAppState extends State {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(children: [
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              onPressed: () {
-                setState(() {
-                  _products.add('Awesome Product');
-                });
-              },
-              child: Text('Add Product'),
-            ),
-          ),
-          Column(
-              children: _products
-                  .map((e) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/noreen.jpg'),
-                            Text(e)
-                          ],
-                        ),
-                      ))
-                  .toList())
-        ]),
+        body: ProductsManager(),
       ),
     );
   }
