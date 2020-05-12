4cc0a9d7e831b038bfe5c6155e0e5b5044114b0d
Gerald Simon <gerald@adroittech.co.tz>
2020-05-12 15:50:53 +0300

    Add theme and pass data up

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'dart:ui';
 
-import 'package:first_app/products_manager.dart';
+import 'package:first_app/product_manager.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange, accentColor: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: ProductsManager(),
+        body: ProductsManager('FOOD TESTER'),
       ),
     );
   }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,8 +1,13 @@
+import 'package:first_app/product_control.dart';
 import 'package:flutter/material.dart';
 
 import './products.dart';
 
 class ProductsManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductsManager(this.startingProduct);
+
   @override
   State<StatefulWidget> createState() {
     return _ProductManagerState();
@@ -10,7 +15,19 @@ class ProductsManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductsManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +35,7 @@ class _ProductManagerState extends State<ProductsManager> {
       children: [
         Container(
           margin: EdgeInsets.all(10.0),
-          child: RaisedButton(
-            onPressed: () {
-              setState(() {
-                _products.add('Awesome Product');
-              });
-            },
-            child: Text('Add Product'),
-          ),
+          child: ProductControl(_addProduct)
         ),
         Products(_products)
       ],
