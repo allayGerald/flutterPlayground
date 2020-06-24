d67f4092c23cbf983252b65c8e35b5e7e70cdbdd
Gerald Simon <gerald@adroittech.co.tz>
2020-06-24 12:57:22 +0300

    Feat user inputs

--- lib/pages/product-create.dart
+++ lib/pages/product-create.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String _titleValue = '';
+  String _description = '';
+  double _price = 0.00;
+
   @override
   Widget build(BuildContext context) {
-  return Center(child: Text('Create Product'),);
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Title'),
+            onChanged: (String value) {
+              setState(() {
+                _titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Price'),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              _price = value as double;
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Description'),
+            maxLines: 4,
+            onChanged: (String value) {
+              setState(() {
+                _description = value;
+              });
+            },
+          )
+        ],
+      ),
+    );
   }
 }
