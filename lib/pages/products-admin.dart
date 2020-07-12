ddc1a711b433929cf76758c90790e8999c046d6f
Gerald Simon <geraldallay@gmail.com>
2020-07-12 13:21:22 +0300

    More refactoring and optimazation

--- lib/pages/products-admin.dart
+++ lib/pages/products-admin.dart
@@ -9,28 +9,32 @@ class ProductsAdminPage extends StatelessWidget {
 
   ProductsAdminPage(this.addProduct, this.deleteProduct);
 
+  Widget _buildSideDrawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text('Choose'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text('All products'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/');
+            },
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-            drawer: Drawer(
-              child: Column(
-                children: <Widget>[
-                  AppBar(
-                    automaticallyImplyLeading: false,
-                    title: Text('Choose'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.shopping_basket),
-                    title: Text('All products'),
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/');
-                    },
-                  )
-                ],
-              ),
-            ),
+            drawer: _buildSideDrawer(context),
             appBar: AppBar(
               title: Text('Manage Products'),
               bottom: TabBar(
--- lib/widgets/products/product-card.dart
+++ lib/widgets/products/product-card.dart
@@ -9,43 +9,51 @@ class ProductCard extends StatelessWidget {
 
   ProductCard(this.product, this.productIndex);
 
+  Widget _buildTitlePriceRow() {
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TitleDefault(product['title']),
+          SizedBox(
+            width: 9,
+          ),
+          PriceTag(product['price'].toString()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionButtons(BuildContext context) {
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          onPressed: () => Navigator.pushNamed<bool>(
+              context, '/product/' + productIndex.toString()),
+          icon: Icon(Icons.info),
+          color: Theme.of(context).accentColor,
+        ),
+        IconButton(
+          onPressed: () => Navigator.pushNamed<bool>(
+              context, '/product/' + productIndex.toString()),
+          icon: Icon(Icons.favorite_border),
+          color: Colors.red,
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Column(
         children: <Widget>[
           Image.asset(product['image']),
-          Container(
-            padding: EdgeInsets.only(top: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TitleDefault(product['title']),
-                SizedBox(
-                  width: 9,
-                ),
-                PriceTag(product['price'].toString()),
-              ],
-            ),
-          ),
+          _buildTitlePriceRow(),
           AddressTag('Mwenge, Dar-Es-Salaam'),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                onPressed: () => Navigator.pushNamed<bool>(
-                    context, '/product/' + productIndex.toString()),
-                icon: Icon(Icons.info),
-                color: Theme.of(context).accentColor,
-              ),
-              IconButton(
-                onPressed: () => Navigator.pushNamed<bool>(
-                    context, '/product/' + productIndex.toString()),
-                icon: Icon(Icons.favorite_border),
-                color: Colors.red,
-              )
-            ],
-          )
+          _buildActionButtons(context)
         ],
       ),
     );
