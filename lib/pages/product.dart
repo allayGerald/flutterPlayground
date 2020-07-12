8c3044ed6edbb7ca4ac8463501f4b73e4600ba31
Gerald Simon <geraldallay@gmail.com>
2020-07-12 13:10:14 +0300

    Products details page: Show all fields

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:first_app/widgets/products/address_tag.dart';
+import 'package:first_app/widgets/products/price-tag.dart';
 import 'package:first_app/widgets/ui-elements/title-default.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -35,6 +37,25 @@ class ProductPage extends StatelessWidget {
         context: context);
   }
 
+  Widget _buildAddressPriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('Mwenge, Dar Es Salaam',
+            style: TextStyle(fontFamily: 'Oswald', color: Colors.grey)),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          child: Text(
+            '|',
+            style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+          ),
+        ),
+        Text(product['price'].toString() + ' TZS',
+            style: TextStyle(fontFamily: 'Oswald', color: Colors.grey))
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -53,6 +74,14 @@ class ProductPage extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.all(10.0),
                   child: TitleDefault(product['title']),
+                ),
+                _buildAddressPriceRow(),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    product['description'],
+                    textAlign: TextAlign.center,
+                  ),
                 ),
                 Container(
                     padding: EdgeInsets.all(10.0),
